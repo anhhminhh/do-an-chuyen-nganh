8fa20784c6e0040e483642a9c502035c76903e99
anhhminhh <58257552+anhhminhh@users.noreply.github.com>
2021-11-15 16:49:59 +0700

    Add files via upload

---
+++ layouts/menugiua.php
@@ -0,0 +1,35 @@
+<div class="wrapper">
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="index.php  ?>" class="nav-link">Trang chủ</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="https://www.facebook.com/AnhhMinnh/" class="nav-link">Liên hệ</a>
+      </li>
+    </ul>
+
+    <!-- SEARCH FORM -->
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Messages Dropdown Menu -->
+      <li class="nav-item">
+        <div class="user-panel mt-1 pb-1 mb-1 d-flex">
+          <div class="info">
+          <a class="d-block">xin chào, Phan Anh Minh</a>
+        </div>
+        <div class="image">
+          <img src="dist/img/test.jpg" alt="User Image">
+        </div>
+        
+      </div>
+      </li>
+    </ul>
+  </nav>
+</div>
