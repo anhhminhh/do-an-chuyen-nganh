a22e28d2d9186c049d93514674c957bd032ba06f
nguyenluuductai1012 <56686114+nguyenluuductai1012@users.noreply.github.com>
2021-11-15 17:30:21 +0700

    Add files via upload

---
+++ layouts/footerpage.php
@@ -0,0 +1,38 @@
+<div id="contact" class="footer">
+    <div class="container">
+        <div class="row pdn-top-30">
+            <div class="col-md-12 ">
+                <div class="footer-box">
+                    <div class="headinga">
+                        <h3>Address</h3>
+                        <span>Healing Center, 176 W Streetname,New York, NY 10014, US</span>
+                        <p>(+71) 8522369417
+                            <br>demo@gmail.com</p>
+                    </div>
+                    <ul class="location_icon">
+                        <li> <a href="#"><i class="fab fa-facebook"></i></i></a></li>
+                        <li> <a href="#"><i class="fab fa-twitter"></i></a></li>
+                        <li> <a href="#"><i class="fab fa-instagram"></i></a></li>
+
+                    </ul>
+                    <div class="menu-bottom">
+                        <ul class="link">
+                            <li> <a href="index.php">Trang chủ</a></li>
+                            <li> <a href="about.php">Giới thiệu</a></li>
+
+                            <li> <a href="brand.php">Thương hiệu </a></li>
+                            <li> <a href="news.php">Tin tức  </a></li>
+                            <li> <a href="contact.php">Liên hệ</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <div class="container">
+            <p><i class="far fa-copyright"></i><?php echo date("Y"); ?> All Rights Reserved. Design By<a href="https://html.design/"> Free Html Templates</a></p>
+        </div>
+    </div>
+</div>
+
---
+++ layouts/navbar.php
@@ -0,0 +1,50 @@
+<div class="header" style="font-family: Arial;">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                <div class="full">
+                    <div class="center-desk">
+                        <div class="logo">
+                            <a href="index.php"><img src="images/logo.png" alt="#"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                <div class="menu-area">
+                    <div class="limit-box">
+                        <nav class="main-menu">
+                            <ul class="menu-area-main">
+                                <li class="active"> <a href="index.php"><i class="fas fa-home"></i>&nbsp;Trang chủ</a> </li>
+                                <li class="active">
+                                    <a href="brand.php"><i class="fas fa-store"></i>&nbsp;Thương hiệu</a>
+                                    <ul class="dropdown-thuonghieu">
+                                        <li class="active1"><a href="brand.php">IPHONE</a></li>
+                                        <li class="active1"><a href="brand.php">SAMSUNG</a></li>
+                                        <li class="active1"><a href="brand.php">OPPO</a></li>
+                                        <li class="active1"><a href="brand.php">VIVO</a></li>
+                                        <li class="active1"><a href="brand.php">XIAOMI</a></li>
+                                        <li class="active1"><a href="brand.php">REALME</a></li>
+                                        <li class="active1"><a href="brand.php">NOKIA</a></li>
+                                        <li class="active1"><a href="brand.php">MOBELL</a></li>
+                                        <li class="active1"><a href="brand.php">ITEL</a></li>
+                                    </ul>
+                                </li>
+                                <li class="active"><a href="news.php"><i class="far fa-newspaper"></i>&nbsp;Tin tức</a></li>
+                                <li class="active"><a href="contact.php"><i class="fas fa-phone-alt"></i>&nbsp;Liên hệ</a></li>
+
+                                <li class="active">
+                                    <a href="#"><i class="fas fa-user"></i>&nbsp;Tài khoản</a>
+                                </li>
+                                <li>
+                                    <a href="giohang.php"><i class="fas fa-shopping-cart"></i></a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
